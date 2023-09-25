86daac6c93384b8def2ea57fc8329d623ed4394d
julia.s.eller@unesp.br <39045611+gabrielribeirof@users.noreply.github.com>
2023-09-25 17:22:51 -0300

    Commit 6

--- teste.c
+++ teste.c
@@ -12,6 +12,18 @@ int Sub(int n1, int n2){
     return subtracao;
 }
 
+int Mult(int n1, int n2){
+    int mult;
+    mult = n1*n2;
+    return mult;
+}
+
+int Div(int n1, int n2){
+    int divisao;
+    divisao = n1/n2;
+    return divisao;
+}
+
 int main (){
     int num1, num2;
     
@@ -22,6 +34,7 @@ int main (){
    
     printf("SOMA: %d\n", Add(num1, num2));
     printf("SUBTRACAO: %d\n", Sub(num1, num2));
-
+    printf("MULTIPLICACAO: %d\n", Mult(num1, num2));
+    printf("DIVISAO: %d\n", Div(num1, num2));
     return 0;
 }
